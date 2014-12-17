f419b58323030e1a351aa44b7d20222a2e96c624
Colin Wood <cwood06@gmail.com>
2014-12-17 15:24:35 -0800

    Fix issue where auto.master is not made.
    When you just create mounts in mapfiles they auto.master is not
    made. So we will check to see if it is defined. If it isnt create the
    fragment. Then update it with the new mount point.

--- manifests/mount.pp
+++ manifests/mount.pp
@@ -7,6 +7,7 @@ define autofs::mount (
   $options    = undef,
   $mapfile    = undef,
   $order      = undef,
+  $mapfile_options = undef
 ) {
   include autofs
   include autofs::params
@@ -24,6 +25,21 @@ define autofs::mount (
     mapfile => $mapfile_real,
     content => $content,
     order   => $order,
+  }
+
+  if $mapfile {
+    if !defined(Autofs::Mapfile["autofs::mount ${autofs::params::master}"]) {
+      autofs::mapfile { "autofs::mount ${autofs::params::master}":
+        path => $autofs::params::master
+      }
+    }
+
+    concat::fragment { "autofs::mount master ${path}:${mountpoint}":
+      ensure  => $ensure,
+      target  => $autofs::params::master,
+      content => "${title} ${path} ${mapfile_options}",
+    }
+
   }
 
 }
